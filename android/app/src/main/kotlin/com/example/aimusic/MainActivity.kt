b4710795941105760172c1075f7ce88f5fd2dce1
Nipun Goel <goelnipun2525@gmail.com>
2021-10-18 19:08:31 +0530

    setup

---
+++ android/app/src/main/kotlin/com/example/aimusic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aimusic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/aiutils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+mixin AIColors {
+  static Color primarycolor = Vx.orange400;
+  static Color primarycolor2 = Vx.purple500;
+}
